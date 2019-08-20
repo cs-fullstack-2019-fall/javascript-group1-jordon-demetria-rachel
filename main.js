ffadf13fdbea40f9cd185d7631fad924259d2d41
jordondoug2019 <jdouglas2015@gmail.com>
2019-08-20 11:08:57 -0500

    englishArray

--- main.js
+++ main.js
@@ -3,11 +3,15 @@ let userInput= prompt (`Press 1. To list English to Spanish translation
 Press 2. To list all Spanish to English Translation  Press 3. To Enter a new English Translation
  Press 4. To enter a New Spanish Translation Press q to quit`);
 
-let englishArray=["One","two","three","red","blue","green"];
+let englishArray=[];
+let spanishArray=[];
 
 if (userInput==="1")
 {
-    alert()
+    for(i=0;i<englishArray.length;i++)
+    {
+        console.log(`The english ${englishArray[i]} is ${spanishArray[i]} `)
+    }
 }
 if(userInput==="2")
 {
@@ -22,4 +26,4 @@ if(userInput==="4")
 {
 
 }
-else if (userInput==="q")
+else if (userInput==="q");
